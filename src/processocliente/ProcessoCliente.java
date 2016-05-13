a7fa5cbb4db69b63586c0f9ada0997299d758bf8
Renan <renan.taizo@gmail.com>
2016-05-12 22:43:27 -0300

    adicionado compra de hospedagem

--- src/processocliente/ProcessoCliente.java
+++ src/processocliente/ProcessoCliente.java
@@ -56,7 +56,11 @@ public class ProcessoCliente {
             MENU1 += "Opção: ";
             System.out.print(MENU1);
 
+            int opt1, opt3, opt5;
+            String opt2, opt4, compra;
+            
             switch (scanner.nextLine()){
+                
                 case "1":
                     String passagem = "";
                     for (int i = 0; i < server.listaPassagens().length; i++){
@@ -64,13 +68,18 @@ public class ProcessoCliente {
                         System.out.println((i+1) + ". " + passagem );
                     }
                     System.out.print("\n\nSelecione a passagem ou 0 para sair: ");
-                    int opt1 = scanner.nextInt();
+                    opt1 = scanner.nextInt();
                     scanner.nextLine();
+                    
                     if (opt1 == 0)
                         break;
+                    else opt1--;    //corrige valor de opt pra bater com indice de array
+                    
+                    passagem = (String) server.listaPassagens()[opt1];
+                    
                     System.out.print("\nSomente ida? [s/n]: ");
 //                    scanner.nextLine();
-                    String opt2 = scanner.nextLine();
+                    opt2 = scanner.nextLine();
                     String dIda = "";
                     String dVolta = "";
                     boolean somenteIda = true;
@@ -85,7 +94,7 @@ public class ProcessoCliente {
                         
                     System.out.print("\nDigite o número de passageiros: [1-9]: ");
 //                    scanner.nextLine();
-                    int opt3 = scanner.nextInt();
+                    opt3 = scanner.nextInt();
                     
                     int []idades = new int[opt3];
                     for(int c = 0; c < opt3; c++){
@@ -94,20 +103,84 @@ public class ProcessoCliente {
                     }
                     scanner.nextLine();
                     System.out.print("\nDigite os 16 digitos do cartão: ");
-                    String opt4 = scanner.nextLine();
+                    opt4 = scanner.nextLine();
                     
-                    String compra = "\n** Dados da compra **\n"
-                            + passagem + "\n" 
+                    compra = passagem + "\n" 
                             + "Saída: " + dIda + "\n"
                             + "Retorno: " + dVolta + "\n"
                             + "Qtde. Passageiros: " + opt3 + "\n"
                             + "Dados do Cartão : " + opt4 + "\n\n";
                     
-                    System.out.print(compra);
-                    voosComprados.add(compra);
+                    if(server.compraPassagem(opt1)){
+                        System.out.println("\n*************** Dados da compra ***************\n");
+                        System.out.print(compra);
+                        System.out.println("\n***********************************************\n");
+                        voosComprados.add(compra);
+                    } else {
+                        System.out.println("\n\nNão foi possivel efetuar a compra. Tente novamente.\n");
+                    }
                     
                     break;
                 case "2":
+                    String hospedagem = "";
+                    String quartos = "";
+                    
+                    for (int i = 0; i < server.listaHospedagens().length; i++){
+                        hospedagem = (String)server.listaHospedagens()[i];
+                        System.out.println((i+1) + ". " + hospedagem );
+                    }
+                    System.out.print("\n\nSelecione a hospedagem ou 0 para sair: ");
+                    opt1 = scanner.nextInt();
+                    scanner.nextLine();
+                    
+                    if (opt1 == 0)
+                        break;
+                    else opt1--;    //corrige pra bater com indice do array
+                    
+                    hospedagem = (String) server.listaHospedagens()[opt1];
+                    int index = hospedagem.indexOf(", com");
+                    quartos = hospedagem.substring(index+6, index+7);
+                    
+                    String dEntrada = "";
+                    String dSaida = "";
+                    
+                    System.out.print("Check-in [dd/MM/yyyy]: ");
+                    dEntrada = scanner.nextLine();
+                    
+                    System.out.print("Check-out [dd/MM/yyyy]: ");
+                    dVolta = scanner.nextLine();
+                        
+                    System.out.print("\nDigite o número de quartos: [1-"+quartos+"]: ");
+//                    scanner.nextLine();
+                    opt3 = scanner.nextInt();
+                    
+                    System.out.print("\nDigite o número de hospedes: [1-"+ 2*opt3 +"]: ");
+                    opt5 = scanner.nextInt();
+                    
+                    int []hospedes = new int[opt5];
+                    for(int c = 0; c < opt5; c++){
+                        System.out.print("\nIdade do hospede "+c+": ");
+                        hospedes[c] = scanner.nextInt();
+                    }
+                    scanner.nextLine();
+                    System.out.print("\nDigite os 16 digitos do cartão: ");
+                    opt2 = scanner.nextLine();
+                    
+                    compra = hospedagem + "\n" 
+                            + "Entrada: " + dEntrada + "\n"
+                            + "Saída: " + dSaida + "\n"
+                            + "N. de Quartos: " + opt3 + "\n"
+                            + "Qtde. hospedes: " + opt5 + "\n"
+                            + "Dados do Cartão : " + opt2 + "\n\n";
+                    
+                    if(server.compraHospedagem(opt1)){
+                        System.out.println("\n*************** Dados da compra ***************\n");
+                        System.out.print(compra);
+                        System.out.println("\n***********************************************\n");
+                        hospedagensCompradas.add(compra);
+                    } else {
+                        System.out.println("\n\nNão foi possivel efetuar a compra. Tente novamente.\n");
+                    }
                     break;
                 case "3":
                     break;
