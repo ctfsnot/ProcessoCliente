3a6d0dd59aac3f7ab15492f5d5256d9cbecb69e8
Renan <renan.taizo@gmail.com>
2016-05-12 21:43:44 -0300

    upd

--- src/processocliente/ProcessoCliente.java
+++ src/processocliente/ProcessoCliente.java
@@ -13,6 +13,7 @@ import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 /**
@@ -24,6 +25,8 @@ public class ProcessoCliente {
     static Registry servidorNomes;
     static InterfaceServidor server;
     static String pid;
+    static ArrayList<String> voosComprados = new ArrayList();
+    static ArrayList<String> hospedagensCompradas = new ArrayList();
     
     public static void main(String[] args) throws RemoteException, NotBoundException, MalformedURLException {
         
@@ -55,10 +58,10 @@ public class ProcessoCliente {
 
             switch (scanner.nextLine()){
                 case "1":
-                    
+                    String passagem = "";
                     for (int i = 0; i < server.listaPassagens().length; i++){
-                        String result = (String)server.listaPassagens()[i];
-                        System.out.println((i+1) + ". " + result );
+                        passagem = (String)server.listaPassagens()[i];
+                        System.out.println((i+1) + ". " + passagem );
                     }
                     System.out.print("\n\nSelecione a passagem ou 0 para sair: ");
                     int opt1 = scanner.nextInt();
@@ -68,7 +71,18 @@ public class ProcessoCliente {
                     System.out.print("\nSomente ida? [s/n]: ");
 //                    scanner.nextLine();
                     String opt2 = scanner.nextLine();
-
+                    String dIda = "";
+                    String dVolta = "";
+                    boolean somenteIda = true;
+                    System.out.print("Data de ida [dd/MM/yyyy]: ");
+                    dIda = scanner.nextLine();
+                    
+                    if(opt2.equals("n")){
+                        somenteIda = false;
+                        System.out.print("Data de volta [dd/MM/yyyy]: ");
+                        dVolta = scanner.nextLine();
+                    }
+                        
                     System.out.print("\nDigite o número de passageiros: [1-9]: ");
 //                    scanner.nextLine();
                     int opt3 = scanner.nextInt();
@@ -82,6 +96,16 @@ public class ProcessoCliente {
                     System.out.print("\nDigite os 16 digitos do cartão: ");
                     String opt4 = scanner.nextLine();
                     
+                    String compra = "\n** Dados da compra **\n"
+                            + passagem + "\n" 
+                            + "Saída: " + dIda + "\n"
+                            + "Retorno: " + dVolta + "\n"
+                            + "Qtde. Passageiros: " + opt3 + "\n"
+                            + "Dados do Cartão : " + opt4 + "\n\n";
+                    
+                    System.out.print(compra);
+                    voosComprados.add(compra);
+                    
                     break;
                 case "2":
                     break;
@@ -94,7 +118,5 @@ public class ProcessoCliente {
                     break;
             }
         }
-        
-        
     }
 }
